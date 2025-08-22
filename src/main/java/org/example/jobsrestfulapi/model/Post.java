7e76c747eadf2b0c10f6f304c0e270c29e234fd8
Tojan-Naiem <tojan050@gmail.com>
2025-08-22 13:26:27 +0300

    feat : add job model class

---
+++ src/main/java/org/example/jobsrestfulapi/model/Post.java
@@ -0,0 +1,77 @@
+package org.example.joblisting.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "JobPost")
+public class Post {
+    @Id
+    private String id;
+    private String profile;
+    private String desc;
+    private int exp;
+    private List<String> teach=new ArrayList<>();
+
+    public Post(String profile,String desc,int exp,List<String>teach) {
+    this.profile=profile;
+    this.desc=desc;
+    this.exp=exp;
+    this.teach=teach;
+    }
+    public Post() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public List<String> getTeach() {
+        return teach;
+    }
+
+    public void setTeach(List<String> teach) {
+        this.teach = teach;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "profile='" + profile + '\'' +
+                ", desc='" + desc + '\'' +
+                ", exp=" + exp +
+                ", teach=" + teach +
+                '}';
+    }
+}
