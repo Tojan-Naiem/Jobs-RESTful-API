e527aa48b4ed89f510136bd3c424416e16f484ab
Tojan-Naiem <tojan050@gmail.com>
2025-08-22 13:23:46 +0300

    init project

---
+++ src/main/java/org/example/jobsrestfulapi/JobsResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobsrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsResTfulApiApplication.class, args);
+	}
+
+}
