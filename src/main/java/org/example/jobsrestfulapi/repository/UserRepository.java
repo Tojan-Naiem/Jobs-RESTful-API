5cb0910b9da6904a7c7f4a169e6c5bb501516cdc
Tojan_Naiem <tojan050@gmail.com>
2025-08-26 15:19:20 +0300

    feat : add role and user repository

---
+++ src/main/java/org/example/jobsrestfulapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package org.example.jobsrestfulapi.repository;
+
+import org.example.jobsrestfulapi.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+     Optional<User> findByUsername(String username);
+     Optional<User> findByEmail(String email);
+}
